444894a5fb32e7ad3f65755b87e9edfff2ea5318
Alex-Yu21 <yualex2112@gmail.com>
2025-06-18 20:55:10 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bhuvih HR Solutions',
+      debugShowCheckedModeBanner: false,
+      home: const RootScreen(),
+    );
+  }
+}
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
